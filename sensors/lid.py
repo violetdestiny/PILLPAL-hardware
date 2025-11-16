0c2e8c9d5565cd0a3f43065111ca735b8e71a9fd
hypzx2105 <eliasarza21@gmail.com>
2025-11-16 21:57:14 +0000

    feat: add lid detection sensor module

---
+++ sensors/lid.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+LID_PIN = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LID_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def is_lid_open():
+    return GPIO.input(LID_PIN) == GPIO.LOW
+
+def detect_lid_open(callback):
+    last_state = is_lid_open()
+    while True:
+        current_state = is_lid_open()
+        if current_state != last_state:
+            if current_state:
+                callback()
+        last_state = current_state
+        time.sleep(0.1)
