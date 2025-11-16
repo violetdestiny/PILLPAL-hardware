372d3a3a3e474bd7bae4ef72c6a2d7e9db8ae17f
hypzx2105 <eliasarza21@gmail.com>
2025-11-16 23:26:12 +0000

    fix: correct indentation in lid detection logic

--- sensors/lid.py
+++ sensors/lid.py
@@ -6,13 +6,13 @@ LID_PIN = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LID_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def is_lid_open():
-
+def lid_is_open():
     return GPIO.input(LID_PIN) == GPIO.LOW
 
-def detect_lid_open(on_open, on_close):
-    last_state = is_lid_open()
-	while True:
+def detect_lid_events(on_open, on_close):
+    last_state = lid_is_open()
+
+    while True:
         current_state = lid_is_open()
 
         if current_state != last_state:
